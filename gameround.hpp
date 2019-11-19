029a891553f243c07e9924832cf132f6363c38cb
flameousfire <56444272+flameousfire@users.noreply.github.com>
2019-11-19 14:53:08 +0200

    Add files via upload

---
+++ gameround.hpp
@@ -0,0 +1,54 @@
+#ifndef GAMEROUND_HPP
+#define GAMEROUND_HPP
+#include <vector>
+
+struct Player
+{
+    std::string name;
+    int idnumber;
+    int stack;
+    int bet;
+    std::vector<int> hand;
+
+    Player();
+};
+
+extern std::vector<Player> playerlist;
+
+
+struct Gameround
+{
+    int playersnum;
+    int dealer;
+    int pot;
+    std::vector<Player*> roundplayers;
+    int sblind;
+    int bblind;
+    int highestbet;
+    int minraise;
+    int counter;
+    int turnorder;
+    bool firstbid;
+
+
+Gameround();
+void turncaller();
+void smallblind(Player& player);
+void bigblind(Player& player);
+void sturn(Player& who);
+void sraise(Player& who);
+void scall(Player& who);
+void sfold(Player* who);
+void raise(Player& who);
+void call(Player& who);
+void fold(Player* who);
+void turn(Player& who);
+void comparetable();
+void potdivider(int win, std::vector<int> tie);
+void winlosscheck();
+void roundrun();
+
+};
+
+
+#endif
